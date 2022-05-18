0f6d61ea77597401c84b631ce28a72622853fd1e
bitsko <26992310+bitsko@users.noreply.github.com>
2022-05-18 15:14:20 -0700

    Update novo-0.1.2.sh

--- novo-0.1.2.sh
+++ novo-0.1.2.sh
@@ -6,10 +6,9 @@
 # STOP YOUR NODE FIRST
 IFS= read -r -p "stop your node first.  press ctrl+c to exit or any key to continue" anykey
 
-
-
 #turn tx indexing on with "index" as pos. param.
 index=0; if [[ "$@" =~ "index" ]]; then index=1; fi
+
 #version of binary
 vrs="0.1.2"
 tar_gz=novo-"$vrs"-x86_64-linux-gnu.tar.gz
@@ -18,17 +17,12 @@ binary=https://github.com/novobitcoin/novobitcoin-release/releases/download/v"$v
 old_dir="$HOME/.novo-bitcoin"
 new_dir="$HOME/.novo"
 
-if [[ ! -d "$new_dir" ]]; then
-        mkdir -p "$new_dir/bin"     
+if [[ -d "$old_dir" ]]; then
+	cp "$old_dir" "$new_dir"
 fi
 
-if [[ -d "$old_dir" ]]; then
-        cp "$old_dir/novo.conf" "$new_dir/novo.conf"
-        cp "$old_dir/wallet.dat" "$new_dir/wallet.dat"
-        if [[ -f "$old_dir/bin/cfg.json" ]]; then
-                cp "$old_dir/bin/cfg.json" "$new_dir/bin/cfg.json"
-        fi
-        
+if [[ ! -d "$new_dir" ]]; then
+        mkdir -p "$new_dir/bin"
 fi
 
 wget "$binary" && tar -xzvf "$tar_gz"
@@ -36,6 +30,7 @@ wget "$binary" && tar -xzvf "$tar_gz"
 cp novo-"$vrs"/bin/novo-cli "$new_dir/bin/novo-cli"
 cp novo-"$vrs"/bin/novod "$new_dir/bin/novod"
 cp novo-"$vrs"/bin/novo-tx "$new_dir/bin/novo-tx"
+
 rm -r novo-"$vrs"
 rm "$tar_gz"
 
@@ -45,8 +40,11 @@ if [[ ! -f "$new_dir/novo.conf" ]]; then
         echo "port=8666"$'\n'"rpcport=8665"$'\n'"rpcuser=$username"$'\n'"rpcpassword=$rpcpassword" > "$new_dir/novo.conf"
         if [[ "$index" == 1 ]]; then echo "txindex=1" >> "$new_dir/novo.conf"; fi
 fi
+
 echo $'\n'"binaries available in $HOME/.novo/bin"$'\n'
 echo "one way to start the node would be to type:"
 echo "$HOME/.novo/bin/novod --daemon"
 echo $'\n'"check version with:"
 echo "$HOME/.novo/bin/novo-cli --version"
+
+
