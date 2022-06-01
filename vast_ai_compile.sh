b023f299355d6d0397c091339480835ecc31b745
bitsko <26992310+bitsko@users.noreply.github.com>
2022-06-01 14:25:32 -0700

    Create vast_ai_compile.sh

---
+++ vast_ai_compile.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+apt update && apt -y upgrade && \
+apt -y install screen libjansson4 ocl-icd-* \
+       opencl-headers libcurl4-openssl-dev pkg-config libtool autoconf \
+       git build-essential libncurses5-dev autogen automake && \
+git clone https://github.com/Bit90pool/novo-cgminer && \
+cd novo-cgminer && \
+./autogen.sh --enable-opencl && \
+CFLAGS="-O2 -Wall -march=native" ./configure 
+make
+echo "./cgminer -o stratum+tcp://mine.bit90.io:3333 -u youraddy.minername -p password -k diablo"
