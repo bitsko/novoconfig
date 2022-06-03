ac32ee3e796647968fb187baf7f7a9dd78f9f23f
bitsko <26992310+bitsko@users.noreply.github.com>
2022-06-03 14:37:04 -0700

    Update vast_compile_test.sh

--- vast_compile_test.sh
+++ vast_compile_test.sh
@@ -8,10 +8,10 @@ apt update
 DEBIAN_FRONTEND=noninteractive apt -y upgrade
 apt -y install screen libjansson4 ocl-icd-* opencl-headers libcurl4-openssl-dev \
         pkg-config libtool autoconf git build-essential autogen automake \
-        libncurses5-dev 
+        libncurses5-dev bc
 git clone https://github.com/Bit90pool/novo-cgminer        
 cd novo-cgminer 
 ./autogen.sh --enable-opencl 
 CFLAGS="-O2 -Wall -march=native" ./configure
-make
+make -j $(echo "$(nproc) - 1" | bc)
 echo "./cgminer -o stratum+tcp://mine.bit90.io:3333 -u yournovoaddresshere.yourminername -p password -k diablo"
