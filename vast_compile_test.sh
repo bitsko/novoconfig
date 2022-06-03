97d394f622f529db9176edfd144b5d3766060253
bitsko <26992310+bitsko@users.noreply.github.com>
2022-06-03 14:57:52 -0700

    Update vast_compile_test.sh

--- vast_compile_test.sh
+++ vast_compile_test.sh
@@ -12,7 +12,7 @@ apt -y install screen libjansson4 ocl-icd-* opencl-headers libcurl4-openssl-dev 
 git clone https://github.com/Bit90pool/novo-cgminer        
 cd novo-cgminer 
 ./autogen.sh --enable-opencl 
-CFLAGS="-O2 -Wall -march=native" ./configure
+# CFLAGS="-O2 -Wall -march=native" ./configure
 make -j $(echo "$(nproc) - 1" | bc)
 echo "./cgminer -o stratum+tcp://mine.bit90.io:3333 -u yournovoaddresshere.yourminername -p password"
 cd novo-cgminer
