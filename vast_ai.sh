2ba09d1cbc2f5d486673d777dd6cf8a44424409a
bitsko <26992310+bitsko@users.noreply.github.com>
2022-06-03 13:44:58 -0700

    Update vast_ai.sh

--- vast_ai.sh
+++ vast_ai.sh
@@ -4,9 +4,9 @@
 
 # wget https://raw.githubusercontent.com/bitsko/novoconfig/main/vast_ai.sh && chmod +x vast_ai.sh && ./vast_ai.sh
 
-# docker GPU command string                                                                                                          
-apt update && apt upgrade -y && sed -i 's/bionic/focal/g' /etc/apt/sources.list && \
-        apt update && apt upgrade -y && apt install screen libjansson4 ocl-icd-* opencl-headers libcurl4-openssl-dev pkg-config libtool autoconf && \
+# docker GPU command string 
+apt update && apt -y upgrade && sed -i 's/bionic/focal/g' /etc/apt/sources.list && \
+        apt update && DEBIAN_FRONTEND=noninteractive apt -y upgrade && apt -y install screen libjansson4 ocl-icd-* opencl-headers libcurl4-openssl-dev pkg-config libtool autoconf && \
         wget https://github.com/Bit90pool/novo-cgminer/releases/download/v1.0/novo-cgiminer-v1.0-ubuntu-20.04.tar.gz && \
         tar -zxvf novo-cgiminer-v1.0-ubuntu-20.04.tar.gz && cd novo-cgminer && \
 echo    "./cgminer -o stratum+tcp://mine.bit90.io:3333 -u youraddy.yourname -p password -k diablo"
