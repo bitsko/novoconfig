81600bfb0231ded8f00a16b1ca46847b92eb9540
bitsko <26992310+bitsko@users.noreply.github.com>
2022-06-27 09:58:14 -0700

    Update novo-0.2.0.sh

--- novo-0.2.0.sh
+++ novo-0.2.0.sh
@@ -18,29 +18,35 @@ nbinary=https://github.com/novoworks/novo/releases/download/v"$novo_vrs"/"$tar_g
 old_dir="$HOME/.novo-bitcoin"
 new_dir="$HOME/.novo"
 
-
+# check if folder already exists and if so back it up
 if [[ -d "$new_dir" ]]; then
         cp -r "$new_dir" "$new_dir"."$EPOCHSECONDS".backup
-	echo "backing up existing novo install to $new_dir.$EPOCHSECONDS.backup"
+	echo $'\n'"***"$'\n'"backing up existing novo install to $new_dir.$EPOCHSECONDS.backup"$'\n'"***"$'\n'
 fi
 
+# check if early version exists and copy over the blockchain and files
 if [[ -d "$old_dir" ]]; then
 	cp -r "$old_dir" "$new_dir"
 fi
 
+# check if the new directory does not exist and if so make it
 if [[ ! -d "$new_dir" ]]; then
         mkdir -p "$new_dir/bin"
 fi
 
+# download the binary
 wget "$nbinary" && tar -xzvf "$tar_gz"
 
+# copy it over to a folder within the novo folder
 cp novo-"$novo_vrs"/bin/novo-cli "$new_dir/bin/novo-cli"
 cp novo-"$novo_vrs"/bin/novod "$new_dir/bin/novod"
 cp novo-"$novo_vrs"/bin/novo-tx "$new_dir/bin/novo-tx"
 
-rm -r novo-"$novo_vrs"
-rm "$tar_gz"
+# remove the archive
+# rm -r novo-"$novo_vrs"
+# rm "$tar_gz"
 
+# if a configuration file for the node does not exist, make one
 if [[ ! -f "$new_dir/novo.conf" ]]; then
         IFS= read -r -p "enter a username for novod"$'\n>' username
         IFS= read -r -p "enter a rpc password for novod"$'\n>' rpcpassword
@@ -49,6 +55,7 @@ if [[ ! -f "$new_dir/novo.conf" ]]; then
         if [[ "$index" == 1 ]]; then echo "txindex=1" >> "$new_dir/novo.conf"; fi
 fi
 
+# if the mempool settings aren't a part of the config file (required for 0.2.0), add them
 if [[ -z $(grep maxmempool "$new_dir/novo.conf") ]]; then
 	echo "maxmempool=1600" >> "$new_dir/novo.conf"
 fi	
