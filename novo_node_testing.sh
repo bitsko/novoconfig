a9541fae0e2a8b73b9fbe054fc4f648c067d938e
bitsko <26992310+bitsko@users.noreply.github.com>
2022-07-14 03:05:12 -0700

    Update novo_node_testing.sh

--- novo_node_testing.sh
+++ novo_node_testing.sh
@@ -133,9 +133,9 @@ if [[ "${armcpu_array[*]}" =~ "$cpu_type" ]] && [[ "$novoBSD" == 0 ]]; then
 elif [[ "${x86cpu_array[*]}" =~ "$cpu_type" ]] && [[ "$novoBSD" == 0 ]]; then
 	./configure --without-gui
 elif [[ "$novoBSD" == 1 ]]; then
+#       CFLAGS="-I/usr/local/include" CXXFLAGS="-I/usr/local/include -I/usr/local/include/db5" \
+#       LDFLAGS="-L/usr/local/lib -L/usr/local/lib/db5" \
 	./configure --without-gui --disable-dependency-tracking --disable-hardening MAKE="gmake" # \
-#     CFLAGS="-I/usr/local/include" CXXFLAGS="-I/usr/local/include -I/usr/local/include/db5" \
-#     LDFLAGS="-L/usr/local/lib -L/usr/local/lib/db5"
 fi
 
 # make
