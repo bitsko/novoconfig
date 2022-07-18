f07f3842134a64d20309bcd25d9cddd5e574ef53
bitsko <26992310+bitsko@users.noreply.github.com>
2022-07-18 07:17:20 -0700

    Update novo_node_testing.sh

--- novo_node_testing.sh
+++ novo_node_testing.sh
@@ -185,7 +185,7 @@ elif [[ "$novoBsd" == 2 ]]; then
 	CFLAGS="-I/usr/local/include -I/usr/include/machine" \
         CXXFLAGS="-I/usr/local/include -I${BDB_PREFIX}/include" \
         LDFLAGS="-L/usr/local/lib -L${BDB_PREFIX}/lib" \
-        BDB_LIBS="-L${BDB_PREFIX}/lib -ldb_cxx-4.8" \
+        BDB_LIBS="-L${BDB_PREFIX}/lib -libdb_cxx-4.8" \
         BDB_CFLAGS="-I${BDB_PREFIX}/include" 
 fi
 if [[ "$?" != 0 ]]; then echo $'\n'"./configure failed"; exit 1; fi
