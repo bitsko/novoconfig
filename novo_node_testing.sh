7c552749b08e3fcac9be5403b6f611dcdce800e2
bitsko <26992310+bitsko@users.noreply.github.com>
2022-07-21 04:16:15 -0700

    Update novo_node_testing.sh

--- novo_node_testing.sh
+++ novo_node_testing.sh
@@ -5,7 +5,7 @@
 # wget -N https://raw.githubusercontent.com/bitsko/novoconfig/main/novo_node_compile.sh && chmod +x novo_node_compile.sh && ./novo_node_compile.sh
 
 progress_banner(){ echo $'\n\n'"${novoTxt} ${debug_step} ${novoTxt}"$'\n\n'; sleep 2; }
-minor_progress(){ echo "*** $debug_step $novoTxt"; sleep 1; }
+minor_progress(){ echo "***** $debug_step *****"; sleep 1; }
 keep_clean(){ if [[ "$frshDir" == 1 ]]; then rm -r "$novoDir" "$novoTgz" 2>/dev/null; fi; }
 
 debug_location(){
