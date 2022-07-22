25ed04a21232b8351c7f595330460c300ed57b52
bitsko <26992310+bitsko@users.noreply.github.com>
2022-07-21 17:58:06 -0700

    Update novo_node_testing.sh

--- novo_node_testing.sh
+++ novo_node_testing.sh
@@ -301,16 +301,18 @@ elif [[ "$novo_OS" == OpenBSD ]]; then
 	#	CFLAGS="-I/usr/local/include -I/usr/include/machine" \
 #################################################################
 elif [[ "$novo_OS" == NetBSD ]]; then
+	export BOOST_ROOT="/usr/pkg/include/boost"
 	./configure --without-gui --disable-dependency-tracking \
 	--disable-hardening \
+	--with-boost=$BOOST_ROOT
 	MAKE=gmake CXX=clang++ CC=clang \
 	CXXFLAGS="-I/usr/pkg/include -I/usr/pkg/include/boost -I/usr/pkg/include/db5" \
 	LDFLAGS="-L/usr/pkg/lib -L/usr/pkg/lib/boost -L/usr/pkg/lib/db5" \
 	BDB_LIBS="-L/usr/pkg/lib -L/usr/pkg/include/db5 -ldb5_cxx -ldb5_cxx-5" \
         BDB_CFLAGS="-I/usr/pkg/include/db5 -I/usr/pkg/lib" \
 	BOOST_VERSION=107800 \
-	BOOST_LIB_VERSION=1_78 \
-	BOOST_ROOT="/usr/pkg/include/boost"
+	BOOST_LIB_VERSION=1_78 
+	
 	debug_location
 	#	CFLAGS="-I/usr/local/include -I/usr/include/machine" \
 ################################################################
