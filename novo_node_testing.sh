4b7b862111385e343cd9d00b58dc86c5d756b1bc
bitsko <26992310+bitsko@users.noreply.github.com>
2022-07-21 17:44:46 -0700

    Update novo_node_testing.sh

--- novo_node_testing.sh
+++ novo_node_testing.sh
@@ -304,7 +304,7 @@ elif [[ "$novo_OS" == NetBSD ]]; then
 	--disable-hardening \
 	MAKE=gmake CXX=clang++ CC=clang \
 	CXXFLAGS="-I/usr/pkg/include -I/usr/pkg/include/boost -I/usr/pkg/include/db5" \
-	LDFLAGS="-L/usr/pkg/lib -L/usr/pkg/lib/db5" \
+	LDFLAGS="-L/usr/pkg/lib -L/usr/pkg/lib/boost -L/usr/pkg/lib/db5" \
 	BDB_LIBS="-L/usr/pkg/lib -L/usr/pkg/include/db5 -ldb5_cxx -ldb5_cxx-5" \
         BDB_CFLAGS="-I/usr/pkg/include/db5 -I/usr/pkg/lib" \
 	BOOST_VERSION=107800 \
