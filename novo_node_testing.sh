c5cd1f1931cde902f72d1be3cd25b20c467e0bfb
bitsko <26992310+bitsko@users.noreply.github.com>
2022-07-21 17:35:54 -0700

    Update novo_node_testing.sh

--- novo_node_testing.sh
+++ novo_node_testing.sh
@@ -307,7 +307,8 @@ elif [[ "$novo_OS" == NetBSD ]]; then
 	LDFLAGS="-L/usr/pkg/lib -L/usr/pkg/lib/db5" \
 	BDB_LIBS="-L/usr/pkg/lib -L/usr/pkg/include/db5 -ldb5_cxx -ldb5_cxx-5" \
         BDB_CFLAGS="-I/usr/pkg/include/db5 -I/usr/pkg/lib" \
-	BOOST_LIBS="-L/usr/pkg/lib"
+	BOOST_VERSION=107800 \
+	BOOST_LIB_VERSION=1_78 
 	debug_location
 	#	CFLAGS="-I/usr/local/include -I/usr/include/machine" \
 ################################################################
