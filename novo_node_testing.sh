c2fcdfd4ce4e8ea672fd3585d0b0ab3785df3fe1
bitsko <26992310+bitsko@users.noreply.github.com>
2022-07-21 18:38:29 -0700

    Update novo_node_testing.sh

--- novo_node_testing.sh
+++ novo_node_testing.sh
@@ -248,20 +248,11 @@ if [[ "$compile_bdb53" == 1 ]]; then
 	tar -zxvf db-5.3.28.tar.gz
 	debug_location; debug_step="configure db-5.3"; minor_progress
 	cd db-5.3.28 || echo "unable to cd to $PWD/db-5.3.28"
-#	sed -i 's/#define	atomic_init(p, val)	((p)->value = (val))/#define	atomic_init_db(p, val)	((p)->value = (val))/g' src/dbinc/atomic.h
-	sed -i 's/__atomic_compare_exchange((p), (o), (n))/__atomic_compare_exchange_db((p), (o), (n))/g' src/dbinc/atomic.h
-	sed -i 's/static inline int __atomic_compare_exchange/static inline int __atomic_compare_exchange_db/g' src/dbinc/atomic.h
-#	sed -i 's/atomic_init(p, (newval)), 1)/atomic_init_db(p, (newval)), 1)/g' src/dbinc/atomic.h
-#	sed -i 's/atomic_init(&alloc_bhp->ref, 1);/atomic_init_db(&alloc_bhp->ref, 1);/g' src/mp/mp_fget.c
-#	sed -i 's/atomic_init(&frozen_bhp->ref, 0);/atomic_init_db(&frozen_bhp->ref, 0);/g' src/mp/mp_mvcc.c
-#	sed -i 's/atomic_init(&alloc_bhp->ref, 1);/atomic_init_db(&alloc_bhp->ref, 1);/g' src/mp/mp_mvcc.c
-#	sed -i 's/atomic_init(&htab[i].hash_page_dirty, 0);/atomic_init_db(&htab[i].hash_page_dirty, 0);/g' src/mp/mp_region.c
-#	sed -i 's/atomic_init(&hp->hash_page_dirty, 0);/atomic_init_db(&hp->hash_page_dirty, 0);/g' src/mp/mp_region.c
-#	sed -i 's/atomic_init(v, newval);/atomic_init_db(v, newval);/g' src/mutex/mut_method.c
-#	sed -i 's/atomic_init(&mutexp->sharecount, 0);/atomic_init_db(&mutexp->sharecount, 0);/g' src/mutex/mut_tas.c
+	debug_step="applying atomic patch"; minor_progress
+	sed -i 's/__atomic_compare_exchange((p), (o), (n))/__atomic_compare_exchange_db((p), (o), (n))/g' src/dbinc/atomic.h; debug_location
+	sed -i 's/static inline int __atomic_compare_exchange/static inline int __atomic_compare_exchange_db/g' src/dbinc/atomic.h; debug_location
 	cd build_unix || echo "unable to cd to $PWD/build_unix"
 	../dist/configure --enable-cxx --prefix=/usr/local --disable-shared --with-pic CC=egcc CXX=eg++ CPP=ecpp
-	# CC=clang CXX=clang++ CPP=clang-cpp
 	debug_location; debug_step="make db5"; minor_progress
 	make
 	debug_location; debug_step="make install db5"; minor_progress
@@ -311,7 +302,6 @@ elif [[ "$novo_OS" == OpenBSD ]]; then
 	BDB_LIBS="-L/usr/local/lib -L/usr/local/BerkeleyDB.5.3/lib -ldb_cxx" \
 	BDB_CFLAGS="-I/usr/local/BerkeleyDB.5.3/include"
 	debug_location
-	#	CFLAGS="-I/usr/local/include -I/usr/include/machine" \
 #################################################################
 elif [[ "$novo_OS" == NetBSD ]]; then
 	export BOOST_ROOT="/usr/pkg/include/boost"
